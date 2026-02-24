256821a15949e637bae0133c469565c3ccdf4043
Elise Blekken <elble0475@uib.no>
2026-02-24 14:20:05 +0100

    GameModel: implemented the player logic into GameModel.java

--- src/main/java/inf112/rocketman/model/GameModel.java
+++ src/main/java/inf112/rocketman/model/GameModel.java
@@ -4,5 +4,42 @@ import inf112.rocketman.controller.ControllableRocketManModel;
 import inf112.rocketman.view.ViewableRocketManModel;
 
 public class GameModel implements ViewableRocketManModel, ControllableRocketManModel {
-    
+    private float playerX = 100f;
+    private float playerY = 300f;
+    private float playerVY = 0f;
+
+    private final float GRAVITY = -1000f;
+    private final float THRUST = 3000f;
+    private final float MAX_VY = 700f;
+
+    private final float PLAYER_H = 64;
+
+    private float worldHeight;
+
+    public GameModel(float worldHeight) {
+        this.worldHeight = worldHeight;
+    }
+
+    public  void update (float dt, boolean thrusting) {
+        float ay = GRAVITY + (thrusting ? THRUST : 0f);
+        playerVY += ay * dt;
+
+        if (playerVY > MAX_VY) playerVY = MAX_VY;
+        if (playerVY < -MAX_VY) playerVY = -MAX_VY;
+
+        playerY += playerVY * dt;
+
+        if (playerY < 0) {
+            playerY = 0;
+            playerVY = 0;
+        }
+
+        if (playerY > worldHeight - PLAYER_H) {
+            playerY = worldHeight - PLAYER_H;
+            playerVY = 0;
+        }
+    }
+
+    public  float getPlayerX() {return playerX; }
+    public float getPlayerY() {return playerY; }
 }
