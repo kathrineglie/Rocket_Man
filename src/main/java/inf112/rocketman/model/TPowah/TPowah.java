9c546bb19564f2a9e9513757c1ac71f5f6a52c09
Kathrine.Lie <kalie5170@uib.no>
2026-03-04 12:06:16 +0100

    Added logic inside the main character file TPowah

--- src/main/java/inf112/rocketman/model/TPowah/TPowah.java
+++ src/main/java/inf112/rocketman/model/TPowah/TPowah.java
@@ -2,9 +2,42 @@ package inf112.rocketman.model.TPowah;
 import com.badlogic.gdx.math.Rectangle;
 
 public class TPowah {
-    private float height, width;
+    private final Rectangle bounds;
+    private float vy = 0;
 
-    public TPowah (Rectangle size) {
+    public TPowah (float x, float y, float width, float height) {
+        this.bounds = new Rectangle(x, y, width, height);
+    }
 
+    public void update(float dt, boolean thrusting, float worldHeight, float thrust, float gravity, float maxVY) {
+        float ay = gravity + (thrusting ? thrust : 0f);
+        vy += ay * dt;
+
+        if (vy > maxVY) vy = maxVY;
+        if (vy < -maxVY) vy = -maxVY;
+
+        bounds.y += vy * dt;
+
+        // Bounds
+        if (bounds.y < 0) {
+            bounds.y = 0;
+            vy = 0;
+        }
+        if (bounds.y > worldHeight - bounds.height) {
+            bounds.y = worldHeight - bounds.height;
+            vy = 0;
+        }
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+
+    public float getY() {
+        return bounds.y;
+    }
+
+    public float getX() {
+        return bounds.x;
     }
 }
