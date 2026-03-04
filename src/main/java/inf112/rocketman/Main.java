c294d6653fe78e5ebca5e9f287fec6f53ab64635
Elise Blekken <elble0475@uib.no>
2026-03-04 10:31:32 +0100

    feature: convert Main to use LibGDX Game and screen architecture

--- src/main/java/inf112/rocketman/Main.java
+++ src/main/java/inf112/rocketman/Main.java
@@ -1,15 +1,23 @@
 package inf112.rocketman;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import inf112.rocketman.controller.RocketManController;
+import inf112.rocketman.view.HomeScreen;
 
-public class Main {
+public class Main extends Game {
+    @Override
+    public void create() {
+        setScreen(new HomeScreen(this));
+    }
+
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("RocketMan");
         config.setWindowedMode(1000, 800);
 
-        new Lwjgl3Application(new RocketManController(), config);
+        new Lwjgl3Application(new Main(), config);
     }
+
 }
---
+++ src/main/java/inf112/rocketman/view/HomeScreen.java
@@ -0,0 +1,44 @@
+package inf112.rocketman.view;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public class HomeScreen implements Screen {
+
+    private final Game game;
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
