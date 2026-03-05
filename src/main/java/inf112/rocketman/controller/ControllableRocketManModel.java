655872ff100454fe056b8c4832b2fa85beac9216
Elise Blekken <elble0475@uib.no>
2026-03-05 13:17:27 +0100

    docs: added a few jaavdocs into interfaces

--- src/main/java/inf112/rocketman/controller/ControllableRocketManModel.java
+++ src/main/java/inf112/rocketman/controller/ControllableRocketManModel.java
@@ -2,18 +2,61 @@ package inf112.rocketman.controller;
 
 import inf112.rocketman.model.GameState;
 
+/**
+ * Interface representing the controllable parts of the RocketMan game model.
+ * It provides methods for updating the game state and managing the game lifecycle
+ * from a controller's perspective.
+ */
 public interface ControllableRocketManModel {
+    /**
+     * updates the game state based on elapsed time and player input.
+     *
+     * @param dt The delta dime (seconds) since last update.
+     * @param thrusting True if the player is currently applying thrust to the rocket.
+     */
+
     void update(float dt, boolean thrusting);
 
+    /**
+     * Retrieves the current state of the game.
+     *
+     * @return The current {@link GameState}.
+     */
     GameState getGameState();
 
+    /**
+     * @return The total height of the game world in world units.
+     */
     float getWorldHeight();
+
+    /**
+     * @return The total width of the game world in world units.
+     */
     float getWorldWidth();
 
+    /**
+     * Transitions the game from the menu/home screen to active playing state.
+     */
     void startGame();
+
+    /**
+     * Transitions the game back to the home screen.
+     */
     void goToHomescreen();
+
+    /**
+     * Pause the ongoing game.
+     */
     void pauseGame();
+
+    /**
+     * Resumes the game form a paused state
+     */
     void resumeGame();
+
+    /**
+     * Signals that the game has ended (e.g., player crashed).
+     */
     void endGame();
 
 }
--- src/main/java/inf112/rocketman/view/TextureProvider.java
+++ src/main/java/inf112/rocketman/view/TextureProvider.java
@@ -3,6 +3,21 @@ package inf112.rocketman.view;
 import com.badlogic.gdx.graphics.Texture;
 import org.w3c.dom.Text;
 
+/**
+ * Interface for providing textures to the view components.
+ * This allows for a decoupled architecture where the renderer does not need
+ * to know how textures are loaded or stored.
+ */
 public interface TextureProvider {
+
+    /**
+     * Retrieves a texture based on its file path or identifier.
+     * Implementation should ideally cache textures to avoid redundant
+     * disk I/O and memory consumption.
+     *
+     * @param name the internal path tp the texture asset (e.g., "Obstacles/Rocket.png")
+     * @return the {@link Texture} associated with the given name,
+     * or null if the texture could not be found
+     */
     Texture getTexture(String name);
 }
