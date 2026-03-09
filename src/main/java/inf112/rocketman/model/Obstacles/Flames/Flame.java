b3d8c1c55a8d096372f9e91b308f88141be53afd
Kathrine.Lie <kalie5170@uib.no>
2026-03-09 16:26:07 +0100

    Added test for the object flame and fixed flame class so it would pass the tests

--- src/main/java/inf112/rocketman/model/Obstacles/Flames/Flame.java
+++ src/main/java/inf112/rocketman/model/Obstacles/Flames/Flame.java
@@ -28,20 +28,12 @@ public class Flame extends Obstacle {
 
     @Override
     public void update(float dt) {
-        x = vx * dt;
+        x += vx * dt;
+        polygon.setPosition(x, y);
     }
 
     public float getAngle() {
         return angle;
-    }
-
-    public void setPolygon(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
-        this.polygon = new Polygon(new float[] {
-                x1, y1,
-                x2, y2,
-                x3, y3,
-                x4, y4
-        });
     }
 
     public Polygon getPolygon() {
